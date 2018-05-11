1e02a3916795f7dde8cbe346618af12da6ed0814
hanmydog <hanmydog@chol.com>
2018-05-11 15:15:43 +0900

    change url of home_hmd

--- zprofile/urls.py
+++ zprofile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^en/$', views.home_en, name='home_en'),
-    url(r'^hmd/$', views.home_hmd, name='home_hmd'),
+    #url(r'^$', views.home, name='home'),
+    #url(r'^en/$', views.home_en, name='home_en'),
+    url(r'^$', views.home_hmd, name='home_hmd'),
 ]
